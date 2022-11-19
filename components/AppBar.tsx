3992f47fad9a55e766c04588da47d1deb8ac3ea6
root <root@DESKTOP-87DPNVK.localdomain>
2022-11-20 01:54:48 +0200

    margin bottom

--- components/AppBar.tsx
+++ components/AppBar.tsx
@@ -6,7 +6,7 @@ import {WalletMultiButton} from '@solana/wallet-adapter-react-ui'
 export const AppBar: FC = () => {
     return (
         <div className={styles.AppHeader}>
-            <Image src="/solanaLogo.png" height={30} width={200} />
+            <Image className={styles.image} src="/solanaLogo.png" height={30} width={200} />
             <span>Send Sol To Any Receiver</span>
             <WalletMultiButton/>
         </div>
